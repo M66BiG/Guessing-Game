6974ca14134473dfa21fcc2b15aebb2b339369d8
Melih aus Beeck <melihesad@hotmail.de>
2023-10-04 13:16:12 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random guess = new Random();
+        int random = guess.Next(1,101);
+        int input;
+        do
+        {
+            Console.WriteLine("Errate die Zahl");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            if (input < random)
+            {
+                Console.WriteLine("Zu niedrig. Versuch es noch einmal.");
+                Thread.Sleep(1000);
+            }
+            else if (input > random)
+            {
+                Console.WriteLine("Zu hoch. Versuch es noch einmal.");
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                Console.WriteLine($"Du hast es geschafft. Die richtige Zahl war: {random}.");
+            }
+        }while (random != input);
+    }
+}
